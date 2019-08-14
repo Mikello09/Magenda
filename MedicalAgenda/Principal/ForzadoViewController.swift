92f8b026670bad14364298f8d1c9a02b31369b1d
Mikel Lopez <mikelsalazarlopez@gmail.com>
2019-08-14 12:19:14 +0200

    forzado viewcontroller created

---
+++ MedicalAgenda/Principal/ForzadoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ForzadoViewController.swift
+//  MedicalAgenda
+//
+//  Created by Mikel Lopez on 08/08/2019.
+//  Copyright © 2019 Mikel Lopez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ForzadoViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
